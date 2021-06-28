d97f7013bcd4e97579e4bc2312ea64f607ff2cd7
粟海林 <suhailin@suhailindeMacBook-Air.local>
2021-06-28 11:42:17 +0800

    大叔方式

--- LittlePinkComm/ViewController.swift
+++ LittlePinkComm/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("dsfafdsafasdf修改")
+        print("dsfafdasf")
     }
 
 
