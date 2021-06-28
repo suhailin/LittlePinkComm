7cc94fe1bc9b5394b5084d35b322f8832bc6323f
粟海林 <suhailin@suhailindeMacBook-Air.local>
2021-06-28 11:29:23 +0800

    init

---
+++ LittlePinkComm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePinkComm
+//
+//  Created by 粟海林 on 2021/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
